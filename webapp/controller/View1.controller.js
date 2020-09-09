f3f926a82afbddc2749f224cc4c64adccbc9911b
Sky Sun <sky.sun01@sap.com>
2020-09-09 07:42:57 +0000

    Initial commit.

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("git_demo.zgit_demo.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"git_demo/zgit_demo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
